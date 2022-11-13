a985c937d4d20d6b6fd960cc0207f507b87a2780
ebo-visio5 <kenvicbernard17@gmail.com>
2022-11-13 15:19:40 -0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const {largeNumber, cut3} = require('./module/app');
+const check = require('./module/mod');
 
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const largestNumber = largeNumber(arr1)
+console.log(largestNumber)
+
+const cut = cut3(arr2)
+console.log(cut)
+
+const add = check.sum(arr1)
+console.log(add)
+
+const join = check.concat(arr1, arr2)
+console.log(join)
